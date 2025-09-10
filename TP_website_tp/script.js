b51a2c3626858fd9c8fce7795507d5baaf447a1b
TP-TechPro <tapiwamuzorori@gmail.com>
2025-09-10 17:48:03 +0200

    Add files via upload

---
+++ TP_website_tp/script.js
@@ -0,0 +1,7 @@
+
+
+// Add event listener to the button
+document.querySelector('button').addEventListener('click', () => {
+    // Navigate to lessons page
+    window.location.href = 'tuturials_page.html';
+});
